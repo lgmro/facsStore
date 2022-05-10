c79bed3caa8034b0193b1801095f941826ac68a4
lgmro <lucasgmmro@gmail.com>
2022-05-10 20:45:15 -0300

    Criação classe produto e metódo main

--- facsStore/src/Produto.java
+++ facsStore/src/Produto.java
@@ -1,13 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author 1272115763
+ * @author Bernardo, Victoria, Lucas Gabriel
  */
-public class Produto {
-    
+public class Produto 
+{
+    private int codigo;
+    private String nome;
+    private String marca;
+    private String cor;
+    private String tipo;
+    private float valor;
+
+    public Produto(int codigo, String nome, String marca, String cor, String tipo, float valor) 
+    {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.marca = marca;
+        this.cor = cor;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public int getCodigo() 
+    {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) 
+    {
+        this.codigo = codigo;
+    }
+
+    public String getNome() 
+    {
+        return nome;
+    }
+
+    public void setNome(String nome) 
+    {
+        this.nome = nome;
+    }
+
+    public String getMarca() 
+    {
+        return marca;
+    }
+
+    public void setMarca(String marca) 
+    {
+        this.marca = marca;
+    }
+
+    public String getCor() 
+    {
+        return cor;
+    }
+
+    public void setCor(String cor) 
+    {
+        this.cor = cor;
+    }
+
+    public String getTipo() 
+    {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) 
+    {
+        this.tipo = tipo;
+    }
+
+    public float getValor() 
+    {
+        return valor;
+    }
+
+    public void setValor(float valor) 
+    {
+        this.valor = valor;
+    }  
 }
