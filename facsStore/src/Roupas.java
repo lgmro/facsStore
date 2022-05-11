8d3a65bbf5c0060ece261f98b56683fab82490e3
BM <bernardomellogs@gmail.com>
2022-05-10 21:03:54 -0300

    Classe Filha Roupas.

---
+++ facsStore/src/Roupas.java
@@ -0,0 +1,29 @@
+public class Roupas extends Produto {
+    
+    private String tamanho,tipoManga;
+
+    public Roupas(String tamanho, String tipoManga, int codigo, String nome, String marca, String cor, String tipo, float valor) {
+        super(codigo, nome, marca, cor, tipo, valor);
+        this.tamanho = tamanho;
+        this.tipoManga = tipoManga;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getTipoManga() {
+        return tipoManga;
+    }
+
+    public void setTipoManga(String tipoManga) {
+        this.tipoManga = tipoManga;
+    }
+    
+    
+    
+}
