532ea1cda71d34e5bdc61e365c6e56b218bc47f1
BM <bernardomellogs@gmail.com>
2022-05-10 21:03:39 -0300

    Classe Filha Calçados.

---
+++ facsStore/src/Calcados.java
@@ -0,0 +1,30 @@
+public class Calcados extends Produto {
+    
+    private int numero;
+    private String materialInterno;
+
+    public Calcados(int numero, String materialInterno, int codigo, String nome, String marca, String cor, String tipo, float valor) {
+        super(codigo, nome, marca, cor, tipo, valor);
+        this.numero = numero;
+        this.materialInterno = materialInterno;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getMaterialInterno() {
+        return materialInterno;
+    }
+
+    public void setMaterialInterno(String materialInterno) {
+        this.materialInterno = materialInterno;
+    }
+    
+    
+    
+}
