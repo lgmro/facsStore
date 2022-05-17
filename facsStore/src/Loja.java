1e1eb136d448dd36ff728e0e6c0987bd7478e660
lgmro <lucasgmmro@gmail.com>
2022-05-17 19:04:05 -0300

    Classe Loja

--- facsStore/src/Loja.java
+++ facsStore/src/Loja.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author 1272115763
+ * @author Bernardo, Lucas Gabriel, Victoria
  */
 public class Loja {
+    private int qtdProduto;
+    Produto[] produtos;
+    
+    public Loja(int qtdProduto) {
+        produtos = new Produto[qtdProduto];
+    }
+    
+    public void addProduto(Produto produto) {
+        for(int i = 0; i < produtos.length; i++) {
+            if(produtos[i] == null) {
+                produtos[i] = produto;
+            }
+        }
+    }
+    
+    
     
     
 }
