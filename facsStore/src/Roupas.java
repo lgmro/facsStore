4825e3e6f3db43d34afa9340f22277f2b1426d70
BM <bernardomellogs@gmail.com>
2022-05-23 22:18:17 -0300

    Comentarios na aba Roupas

--- facsStore/src/Roupas.java
+++ facsStore/src/Roupas.java
@@ -1,12 +1,12 @@
 
 import java.util.Scanner;
 
-public class Roupas extends Produto {
+public class Roupas extends Produto {//Classe para ser um tipo de item
     
     private String tamanho,tipoManga;
     public Roupas(){};
 
-    public Roupas(String tamanho, String tipoManga, int codigo, String nome, String marca, String cor, String tipo, float valor) {
+    public Roupas/*Construtor de roupas*/(String tamanho, String tipoManga, int codigo, String nome, String marca, String cor, String tipo, float valor) {
         super(codigo, nome, marca, cor, tipo, valor);
         this.tamanho = tamanho;
         this.tipoManga = tipoManga;
@@ -26,16 +26,16 @@ public class Roupas extends Produto {
 
     public void setTipoManga(String tipoManga) {
         this.tipoManga = tipoManga;
-    }
+    }//todos os gets e sets
 
-    public void imprimir() {
+    public void imprimir() {//metodo imprime o item tipo roupa
         super.imprimir();
         System.out.println("Tamanho: " + tamanho);
         System.out.println("Tipo de Manga: " + tipoManga);
         System.out.println("------------------------------------ \n");
     }
     
-    public void criarProduto() {
+    public void criarProduto() {//Metodo que cria o produto
         Scanner entrada = new Scanner(System.in);
         super.criarProduto();
         System.out.println("Informe o tamanho: ");
@@ -44,7 +44,7 @@ public class Roupas extends Produto {
         setTipoManga(entrada.nextLine());
     }
 
-    public void alterarProduto(int opcao) {
+    public void alterarProduto(int opcao) {//metado para alterar o produto
         Scanner entrada = new Scanner(System.in);
         super.alterarProduto(opcao);
         if(opcao > 6)
