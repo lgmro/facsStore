ab4e48984cd595ac5b0e9f7b43cffd013de5cee5
BM <bernardomellogs@gmail.com>
2022-05-23 22:15:41 -0300

    Comentarios na aba calçados

--- facsStore/src/Calcados.java
+++ facsStore/src/Calcados.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Calcados extends Produto {
+public class Calcados extends Produto {//Classe para ser um tipo de item
 
     private int numero;
     private String materialInterno;
@@ -11,7 +11,7 @@ public class Calcados extends Produto {
 
     ;
 
-    public Calcados(int numero, String materialInterno, int codigo, String nome, String marca, String cor, String tipo, float valor) {
+    public Calcados/*Construtor do calçados*/(int numero, String materialInterno, int codigo, String nome, String marca, String cor, String tipo, float valor) {
         super(codigo, nome, marca, cor, tipo, valor);
         this.numero = numero;
         this.materialInterno = materialInterno;
@@ -31,16 +31,16 @@ public class Calcados extends Produto {
 
     public void setMaterialInterno(String materialInterno) {
         this.materialInterno = materialInterno;
-    }
+    }//todos os gets e sets
 
-    public void imprimir() {
+    public void imprimir() {//metodo imprime o item tipo calçado
         super.imprimir();
         System.out.println("Número: " + numero);
         System.out.println("Material interno: " + materialInterno);
         System.out.println("------------------------------------ \n");
     }
 
-    public void criarProduto() {
+    public void criarProduto() {//Metodo que cria o produto
         Scanner entrada = new Scanner(System.in);
         super.criarProduto();
         System.out.println("Informe o número: ");
@@ -49,7 +49,7 @@ public class Calcados extends Produto {
         setMaterialInterno(entrada.next());
     }
 
-    public void alterarProduto(int opcao) {
+    public void alterarProduto(int opcao) {//metado para alterar o produto
         Scanner entrada = new Scanner(System.in);
         super.alterarProduto(opcao);
         if(opcao > 6)
